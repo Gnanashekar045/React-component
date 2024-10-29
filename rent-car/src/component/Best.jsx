39864d37c9290cbb5ba5611f997b9ea5cd40f8a7
shanker <gnanashekar045@gmail.com>
2024-10-29 15:25:13 +0530

    project complition

---
+++ rent-car/src/component/Best.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class Best extends Component {
+    render() {
+        return (
+            <section className="best_section">
+                <div className="container">
+                    <div className="book_now">
+                        <div className="detail-box">
+                            <h2>
+                                Our Best Cars
+                            </h2>
+                            <p>
+                                It is a long established fact that a reader will be distracted by the
+                            </p>
+                        </div>
+                        <div className="btn-box">
+                            <a href="">
+                                Book Now
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default Best;
